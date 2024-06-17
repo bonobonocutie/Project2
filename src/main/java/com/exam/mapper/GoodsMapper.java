8fe69509d480c2ac750a4084b12080b132b3ee29
bonobonocutie <bonobonocutie@naver.com>
2024-06-17 13:09:00 +0900

    주석1

---
+++ src/main/java/com/exam/mapper/GoodsMapper.java
@@ -0,0 +1,13 @@
+package com.exam.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.exam.dto.GoodsDTO;
+
+@Mapper
+public interface GoodsMapper {
+	public List<GoodsDTO> goodsList(String gCategory);
+	public GoodsDTO goodsRetrieve(String gCode);
+}
